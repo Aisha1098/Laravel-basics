8ca5b01cb7f4dabefedacb3101305326e75d9cf1
Aisha1098 <aishath.suha17@gmail.com>
2023-04-12 07:40:22 +0500

    ep46

--- blog/app/Http/Controllers/PostController.php
+++ blog/app/Http/Controllers/PostController.php
@@ -2,23 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Category;
 use App\Models\Post;
-use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     
     public function index(){
-        return view('posts',[
-            'posts'=> Post::latest()->filter(request(['search','category']))->get(),
-            'categories' => Category::all(),
-            'currentCategory'=>Category::firstWhere('slug', request('category'))
+        return view('posts.index',[
+            'posts'=> Post::latest()->filter(
+                request(['search','category', 'author'])
+            )->Paginate(6)->withQueryString()
         ]);
     }
 
     public function show(Post $post){
-        return view('post', [
+        return view('post.show', [
             'post' => $post
         ]);
     }
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,35 +1,11 @@
 <?php
 
 use App\Http\Controllers\PostController;
-use App\Models\Category;
-use App\Models\Post;
-use App\Models\User;
-use Illuminate\Foundation\Auth\User as AuthUser;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\Log;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
-use Symfony\Component\VarDumper\VarDumper;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', [PostController::class, 'index'])->name('home');
-
 Route::get('posts/{post:slug}',[PostController::class, 'show']);
 
-Route::get('authors/{author:username}', function (User $author) { 
-    return view('posts', [
-        'posts' => $author->posts,
-        'categories' => Category::all()
-    ]);
-});
+Route::get('register', [RegisterController::class, 'create']);
+Route::post('register', [RegisterController::class, 'store']);
